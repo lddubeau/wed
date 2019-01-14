22d99646c948db69b03752f3914aa3519974ebdf
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-01-14 14:09:08 -0500

    build: version bump

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -6,4 +6,4 @@
  * @copyright Mangalam Research Center for Buddhist Languages
  */
 
-export const version = "5.0.0-alpha.8";
+export const version = "5.0.0-alpha.9";
--- packages/runtime-indexeddb-handler/src/runtime-indexeddb-handler.ts
+++ packages/runtime-indexeddb-handler/src/runtime-indexeddb-handler.ts
@@ -8,7 +8,7 @@ import { Dexie } from "dexie";
 
 import { RuntimeURISchemeHandler } from "@wedxml/default-runtime";
 
-export const version = "5.0.0-alpha.8";
+export const version = "5.0.0-alpha.9";
 
 /**
  * Handler supporting the ``indexeddb`` scheme.
