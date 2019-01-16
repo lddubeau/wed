c1a91c640f1c1d741b47a2aca9402d66079569c6
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-01-16 09:22:15 -0500

    build: version bump

--- packages/runtime-indexeddb-handler/src/runtime-indexeddb-handler.ts
+++ packages/runtime-indexeddb-handler/src/runtime-indexeddb-handler.ts
@@ -8,7 +8,7 @@ import { Dexie } from "dexie";
 
 import { RuntimeURISchemeHandler } from "@wedxml/default-runtime";
 
-export const version = "5.0.0-alpha.13";
+export const version = "5.0.0-alpha.14";
 
 /**
  * Handler supporting the ``indexeddb`` scheme.
